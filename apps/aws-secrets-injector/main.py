6d7630a24f15ac44695552f29533064b795ca009
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-03-22 23:37:12 -0700

    added latest python app for injecting secrets from secrets manager into shared volume

---
+++ apps/aws-secrets-injector/main.py
@@ -0,0 +1,38 @@
+from services.secretsmanager import SecretsManagerService
+import os
+import logging
+import json
+import traceback
+
+logger = logging.getLogger(__name__)
+
+def write_to_file(content, file_dir, file_name):
+    try:
+        if not os.path.exists('file_dir'):
+            os.makedirs('file_dir')
+
+        with open(file_name, "w+") as f:
+            f.write(content)
+    except Exception as ex:
+        traceback.print_exc()
+        logger.error("Failed to write secrets. %s" % ex)
+
+def main():
+
+    try:
+        list_of_secrets_json = os.getenv("AWS_SECRETS", "{}")
+        path = os.getenv("FILE_PATH", "/tmp/")
+        secrets_dict = json.loads(list_of_secrets_json)
+        sm_svc = SecretsManagerService()
+
+        if not secrets_dict == None and not bool(secrets_dict) == False:
+            for key, val in secrets_dict:
+                write_to_file(sm_svc.get_secret(key), path, val)
+
+    except Exception as ex:
+        traceback.print_exc()
+        logger.error("Failed to mount secrets. %s" % ex)
+
+
+if __name__ == "__main__":
+    main()
---
+++ apps/aws-secrets-injector/services/secretsmanager.py
@@ -0,0 +1,42 @@
+import base64
+import boto3
+import threading
+import logging
+import json
+import time
+import os
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+class SecretsManagerService:
+
+    region_name = os.getenv("AWS_REGION", default="ca-central-1")
+    
+    def __init__(self):
+        self.sess = boto3.session.Session()
+        self.client = self.sess.client(service_name="secretsmanager", region_name=self.region_name)
+
+    def get_secret(self, secret_id):
+        try:
+            get_secret_value_response = self.client.get_secret_value(
+            SecretId=secret_id
+        )
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                print("The requested secret " + secret_id + " was not found")
+            elif e.response['Error']['Code'] == 'InvalidRequestException':
+                print("The request was invalid due to:", e)
+            elif e.response['Error']['Code'] == 'InvalidParameterException':
+                print("The request had invalid params:", e)
+            elif e.response['Error']['Code'] == 'DecryptionFailure':
+                print("The requested secret can't be decrypted using the provided KMS key:", e)
+            elif e.response['Error']['Code'] == 'InternalServiceError':
+                print("An error occurred on service side:", e)
+        else:
+            # Secrets Manager decrypts the secret value using the associated KMS CMK
+            # Depending on whether the secret was a string or binary, only one of these fields will be populated
+            if 'SecretString' in get_secret_value_response:
+                return get_secret_value_response['SecretString']
+            else:
+                return base64.b64decode(get_secret_value_response['SecretBinary'])
