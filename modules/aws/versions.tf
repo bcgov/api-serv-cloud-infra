63791e05e251d593a16ae52f75c6a1ca9e4c4d41
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-03-24 15:40:05 -0700

    updated versions

--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -1,6 +1,6 @@
 locals {
     dev_versions = {
-        kong: "2.5.0-f2e9400c"
-        aws-secrets-injector: "0.0.1-f2e9400c"
+        kong: "2.5.0-e5b4f79b"
+        aws-secrets-injector: "0.0.1-e5b4f79b"
     }
 }
