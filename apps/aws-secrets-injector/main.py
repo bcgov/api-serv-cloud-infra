b6b2f4ba65761a2c101a444bfe51e7fe0c864d03
Nithin Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2022-03-24 15:42:40 -0700

    Images/add debugs (#11)
    * added debug statements
    
    * creating a bind mount in an image
    
    * re-building secrets-injector image
    
    * re-building secrets-injector and kong images
    
    * updated versions

--- apps/aws-secrets-injector/main.py
+++ apps/aws-secrets-injector/main.py
@@ -12,11 +12,14 @@ logger = logging.getLogger(__name__)
 
 def write_to_file(content, file_dir, file_name):
     try:
+        logger.debug("Writing to directory %s" % file_dir)
+        logger.debug("Directory exists - %s" % str(os.path.exists(file_dir)))
         if not os.path.exists(file_dir):
             os.makedirs(file_dir)
 
         with open("%s/%s" % (file_dir, file_name), "w+") as f:
             f.write(content)
+        logger.debug("File %s created - %s" % (file_name, str(os.path.isfile("%s/%s" % (file_dir, file_name)))))
     except Exception as ex:
         traceback.print_exc()
         logger.error("Failed to write secrets. %s" % ex)
--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -1,6 +1,6 @@
 locals {
     dev_versions = {
-        kong: "2.5.0-a98fdbdb"
-        aws-secrets-injector: "0.0.1-b96a735d"
+        kong: "2.5.0-e5b4f79b"
+        aws-secrets-injector: "0.0.1-e5b4f79b"
     }
 }
