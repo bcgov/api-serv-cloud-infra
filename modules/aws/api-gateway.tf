273962fc2bf82c3476032514d8dec8b6255fc507
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-06-09 15:41:20 -0700

    fixed issues with config - gateway integration arn and redis subnet group name

--- modules/aws/api-gateway.tf
+++ modules/aws/api-gateway.tf
@@ -9,7 +9,7 @@ resource "aws_apigatewayv2_integration" "aps_kong_gateway_integration" {
   integration_type = "HTTP_PROXY"
 
   integration_method = "ANY"
-  integration_uri    = data.aws_alb.main.id
+  integration_uri    = data.aws_alb.main.arn
   connection_type    = "VPC_LINK"
   connection_id      = aws_apigatewayv2_vpc_link.aps_kong_gateway_vpc_link.id
 }
--- modules/aws/redis.tf
+++ modules/aws/redis.tf
@@ -5,9 +5,10 @@ resource "aws_elasticache_cluster" "kong_redis" {
   num_cache_nodes               = 1
   port                          = var.redis_port_http
   security_group_ids            = [aws_security_group.sg_kong_redis.id]
+  subnet_group_name             = aws_elasticache_subnet_group.kong_redis_subnet_grp.name
 }
 
-# resource "aws_elasticache_subnet_group" "kong_redis_subnet_grp" {
-#   name       = "kong-redis-subnet-grp"
-#   subnet_ids = module.network.aws_subnet_ids.app.ids
-# }
+resource "aws_elasticache_subnet_group" "kong_redis_subnet_grp" {
+  name       = "kong-redis-subnet-grp"
+  subnet_ids = module.network.aws_subnet_ids.app.ids
+}
