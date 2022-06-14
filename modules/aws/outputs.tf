0bb144e521b6835e189e8b90c28520b761ae2f4e
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-06-14 15:59:46 -0700

    using redis cluster as it support auth token

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -8,19 +8,3 @@ output "sns_topic" {
   value       = aws_sns_topic.billing_alert_topic.arn
   description = "Subscribe to this topic using your email to receive email alerts from the budget."
 }
-
-output "redis_address" {
-  value = aws_elasticache_cluster.kong_redis.0.cache_nodes.0.address
-}
-
-output "redis_port" {
-  value = aws_elasticache_cluster.kong_redis.0.port
-}
-
-output "redis_availability_zone" {
-  value = aws_elasticache_cluster.kong_redis.0.availability_zone
-}
-
-output "redis_url" {
-  value = "redis://${aws_elasticache_cluster.kong_redis.0.cache_nodes.0.address}:${aws_elasticache_cluster.kong_redis.0.port}"
-}
--- modules/aws/redis.tf
+++ modules/aws/redis.tf
@@ -1,12 +1,23 @@
-resource "aws_elasticache_cluster" "kong_redis" {
-  count                         = 1
-  cluster_id                    = "kong-redis"
-  engine                        = "redis"
-  node_type                     = "cache.t3.micro"
-  num_cache_nodes               = 1
-  port                          = var.redis_port_http
-  security_group_ids            = [aws_security_group.sg_kong_redis.id]
-  subnet_group_name             = aws_elasticache_subnet_group.kong_redis_subnet_grp.name
+data "aws_secretsmanager_secret" "redis_ratelimit_secret" {
+  name = "plugins-ratelimiting-redis-password"
+}
+
+data "aws_secretsmanager_secret_version" "redis-ratelimit-secret" {
+  secret_id = data.aws_secretsmanager_secret.redis_ratelimit_secret.id
+}
+
+resource "aws_elasticache_replication_group" "kong_redis_cluster" {
+  count                               = 1
+  replication_group_id                = "kong-redis-cluster-grp"
+  replication_group_description       = "Kong Redis Cluster Group"
+  engine                              = "redis"
+  engine_version                      = "6.x"
+  node_type                           = "cache.t3.micro"
+  port                                = 6379
+  transit_encryption_enabled          = true
+  auth_token                          = data.aws_secretsmanager_secret_version.redis-ratelimit-secret.secret_string
+  security_group_ids                  = [aws_security_group.sg_kong_redis.id]
+  subnet_group_name                   = aws_elasticache_subnet_group.kong_redis_subnet_grp.name
 }
 
 resource "aws_elasticache_subnet_group" "kong_redis_subnet_grp" {
