90cf9d31207abdb8794d1689420f4cc8e6299898
Nithin Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2022-06-22 09:21:01 -0700

    Added prometheus image to ECR (#16)
    * adding prometheus image to ecr
    
    * adding prometheus image to ecr with actions
    
    * adding prometheus image to ecr with actions
    
    * created prometheus repo
    
    * added prom version

--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -2,6 +2,6 @@ locals {
     dev_versions = {
         kong: "2.5.0-5a45b007"
         aws-secrets-injector: "0.0.1-e5b4f79b"
-        prometheus: "2.36.2"
+        prometheus: "2.36.2-4861691b"
     }
 }
