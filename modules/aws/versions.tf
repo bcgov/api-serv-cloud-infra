1d4cd3f10e65cc77117f23703682005eda3a95a6
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-06-22 15:41:14 -0700

    added versions of dp query api and prom metrics proxy

--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -3,5 +3,7 @@ locals {
         kong: "2.5.0-5a45b007"
         aws-secrets-injector: "0.0.1-e5b4f79b"
         prometheus: "2.36.2-4861691b"
+        dp-query-api: "830912c0"
+        prom-metrics-proxy: "22e97cf6"
     }
 }
