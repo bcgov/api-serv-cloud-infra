1751e0e0c60ec6c78675ab37674238f9cb534bc1
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-06-22 17:13:58 -0700

    updated the versions

--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -2,8 +2,8 @@ locals {
     dev_versions = {
         kong: "2.5.0-5a45b007"
         aws-secrets-injector: "0.0.1-e5b4f79b"
-        prometheus: "2.36.2-4861691b"
-        dp-query-api: "830912c0"
-        prom-metrics-proxy: "22e97cf6"
+        prometheus: "2.36.2-23427125"
+        dp-query-api: "23427125"
+        prom-metrics-proxy: "23427125"
     }
 }
