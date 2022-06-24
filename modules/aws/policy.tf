a29d671a10aafaa9748b73282722efddf10c41e8
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-06-24 11:14:14 -0700

    removed managed policies as it does not apply custom managed policies

--- modules/aws/policy.tf
+++ modules/aws/policy.tf
@@ -14,75 +14,56 @@ data "aws_iam_policy_document" "ecs_task_assume_role_policy_doc" {
 }
 
 resource "aws_iam_policy" "ssm_get_params_policy" {
-  name   = "SystemsManagerGetParams"
+  name = "SystemsManagerGetParams"
   policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "ssm:GetParameters"
-              ],
-              "Resource": [
-                  "arn:aws:ssm:*:*:parameter/otel-collector-config"
-              ]
-          }
-      ]
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameters"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:*:*:parameter/otel-collector-config"
+        ]
+      }
+    ]
   })
 }
-
-# resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm_get_params_attach" {
-#   role = aws_iam_role.ecs_prom_task_execution_role.id
-#   policy_arn = aws_iam_policy.ssm_get_params_policy.arn
-# }
 
 resource "aws_iam_policy" "ecs_task_cwlogs_policy" {
   name = "CloudWatchCreateLogGroup"
   policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "logs:CreateLogGroup"
-              ],
-              "Resource": [
-                  "arn:aws:logs:*:*:*"
-              ]
-          }
-      ]
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup"
+        ],
+        "Resource" : [
+          "arn:aws:logs:*:*:*"
+        ]
+      }
+    ]
   })
 }
-
-# resource "aws_iam_role_policy_attachment" "ecs_task_execution_cwlogs_attach" {
-#   role = aws_iam_role.ecs_task_execution_role.id
-#   policy_arn = aws_iam_policy.ecs_task_cwlogs_policy.arn
-# }
-
-# Attaching task execution and read from secrets manager policies to task execution role
-# resource "aws_iam_role_policy_attachment" "ecs_task_exec_role_attach" {
-#   role       = aws_iam_role.ecs_task_execution_role.name
-#   for_each = toset([
-#     "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-#   ])
-#   policy_arn = each.value
-# }
 
 resource "aws_iam_policy" "ecs_task_manage_logs_policy" {
   name = "CloudWatchManageLogs"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": [
+        "Effect" : "Allow",
+        "Action" : [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
           "logs:DescribeLogStreams"
         ],
-        "Resource": [
+        "Resource" : [
           "arn:aws:logs:*:*:*"
         ]
       }
@@ -94,14 +75,14 @@ resource "aws_iam_policy" "ecs_task_get_secrets_policy" {
   name = "SecretsManagerGetSecrets"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": [
+        "Effect" : "Allow",
+        "Action" : [
           "secretsmanager:GetSecretValue"
         ],
-        "Resource": [
+        "Resource" : [
           "arn:aws:secretsmanager:ca-central-1:648498837764:secret:*"
         ]
       }
@@ -109,25 +90,15 @@ resource "aws_iam_policy" "ecs_task_get_secrets_policy" {
   })
 }
 
-# resource "aws_iam_role_policy_attachment" "ecs_kong_task_manage_logs_attach" {
-#   role = aws_iam_role.ecs_kong_task_role.name
-#   policy_arn = aws_iam_policy.ecs_task_manage_logs_policy.arn
-# }
-
-# resource "aws_iam_role_policy_attachment" "ecs_kong_get_secrets_attach" {
-#   role = aws_iam_role.ecs_kong_task_role.name
-#   policy_arn = aws_iam_policy.ecs_task_get_secrets_policy.arn
-# }
-
 resource "aws_iam_policy" "ecs_task_read_resources_policy" {
   name = "ECSReadResources"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Action": [
+        "Effect" : "Allow",
+        "Action" : [
           "ecs:ListTasks",
           "ecs:ListServices",
           "ecs:DescribeContainerInstances",
@@ -135,18 +106,46 @@ resource "aws_iam_policy" "ecs_task_read_resources_policy" {
           "ecs:DescribeTasks",
           "ecs:DescribeTaskDefinition"
         ],
-        "Resource": "*"
+        "Resource" : "*"
       }
     ]
   })
 }
 
-# resource "aws_iam_role_policy_attachment" "ecs_prom_task_manage_logs_attach" {
-#   role = aws_iam_role.ecs_prom_task_role.name
-#   policy_arn = aws_iam_policy.ecs_task_manage_logs_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_role_attach" {
+  role = aws_iam_role.ecs_task_execution_role.name
+  for_each = toset([
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    aws_iam_policy.ecs_task_cwlogs_policy.arn
+  ])
+  policy_arn = each.value
+}
 
-# resource "aws_iam_role_policy_attachment" "ecs_prom_task_read_resources_attach" {
-#   role = aws_iam_role.ecs_prom_task_role.name
-#   policy_arn = aws_iam_policy.ecs_task_read_resources_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "ecs_kong_task_role_attach" {
+  role = aws_iam_role.ecs_kong_task_role.name
+  for_each = toset([
+    aws_iam_policy.ecs_task_manage_logs_policy.arn,
+    aws_iam_policy.ecs_task_get_secrets_policy.arn
+  ])
+  policy_arn = each.value
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_prom_task_exec_role_attach" {
+  role = aws_iam_role.ecs_prom_task_execution_role.name
+  for_each = toset([
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+    aws_iam_policy.ssm_get_params_policy.arn
+  ])
+  policy_arn = each.value
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_prom_task_role_attach" {
+  role = aws_iam_role.ecs_prom_task_role.name
+  for_each = toset([
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+    aws_iam_policy.ecs_task_manage_logs_policy.arn,
+    aws_iam_policy.ecs_task_read_resources_policy.arn
+  ])
+  policy_arn = each.value
+}
--- modules/aws/roles.tf
+++ modules/aws/roles.tf
@@ -2,41 +2,23 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "DefaultECSTaskExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy_doc.json
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-    aws_iam_policy.ecs_task_cwlogs_policy.arn
-  ]
-  tags = local.common_tags
+  tags               = local.common_tags
 }
 
 resource "aws_iam_role" "ecs_kong_task_role" {
   name               = "ECSKongTaskRole"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy_doc.json
-  managed_policy_arns = [
-    aws_iam_policy.ecs_task_manage_logs_policy.arn,
-    aws_iam_policy.ecs_task_get_secrets_policy.arn
-  ]
-  tags = local.common_tags
+  tags               = local.common_tags
 }
 
 resource "aws_iam_role" "ecs_prom_task_execution_role" {
   name               = "ECSPrometheusTaskExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy_doc.json
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
-    aws_iam_policy.ssm_get_params_policy.arn
-  ]
-  tags = local.common_tags
+  tags               = local.common_tags
 }
 
 resource "aws_iam_role" "ecs_prom_task_role" {
   name               = "ECSPrometheusTaskRole"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy_doc.json
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
-    aws_iam_policy.ecs_task_manage_logs_policy.arn,
-    aws_iam_policy.ecs_task_read_resources_policy.arn
-  ]
-  tags = local.common_tags
+  tags               = local.common_tags
 }
