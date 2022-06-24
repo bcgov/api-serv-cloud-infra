299590409acdb60b30df711cbdec80f43e1c328b
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-06-24 16:13:50 -0700

    updated the kong image version

--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -1,9 +1,9 @@
 locals {
-    dev_versions = {
-        kong: "2.5.0-5a45b007"
-        aws-secrets-injector: "0.0.1-e5b4f79b"
-        prometheus: "2.36.2-23427125"
-        dp-query-api: "23427125"
-        prom-metrics-proxy: "23427125"
-    }
+  dev_versions = {
+    kong : "2.5.0-6395c3bc"
+    aws-secrets-injector : "0.0.1-e5b4f79b"
+    prometheus : "2.36.2-23427125"
+    dp-query-api : "23427125"
+    prom-metrics-proxy : "23427125"
+  }
 }
